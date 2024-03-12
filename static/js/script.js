4b7e42e12369e3e9b21b273d727d346223b5c26c
Britneyks <britneyzhcn@gmail.com>
2024-03-12 17:04:38 +0800

    20240312提交

---
+++ static/js/script.js
@@ -0,0 +1,199 @@
+console.log('%cCopyright © 2024 子非鱼🐟', 'background-color: #ff00ff; color: white; font-size: 24px; font-weight: bold; padding: 10px;');
+console.log('%c   /\\_/\\', 'color: #8B4513; font-size: 20px;');
+console.log('%c  ( o.o )', 'color: #8B4513; font-size: 20px;');
+console.log(' %c  > ^ <', 'color: #8B4513; font-size: 20px;');
+console.log('  %c /  ~ \\', 'color: #8B4513; font-size: 20px;');
+console.log('  %c/______\\', 'color: #8B4513; font-size: 20px;');
+
+
+
+document.addEventListener('contextmenu', function(event) {
+  event.preventDefault(); // 阻止默认的上下文菜单行为
+});
+
+
+
+
+function toggleClass(selector, className) {
+    var elements = document.querySelectorAll(selector);
+    elements.forEach(function (element) {
+        element.classList.toggle(className);
+    });
+}
+
+function wx(imageURL) {
+    var tcMainElement = document.querySelector(".tc-img");
+    if (imageURL) {
+        tcMainElement.src = imageURL;
+    }
+    toggleClass(".tc-main", "active");
+    toggleClass(".tc", "active");
+
+
+
+}
+
+
+function left() {
+    toggleClass(".left-main", "left-main-open");
+    toggleClass(".left", "left-open");
+
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+
+
+    var themeState = getCookie("themeState") || "Blue";
+    const htmlTag = document.querySelector('html');
+    var svgItems = document.getElementsByTagName("svg");
+    var tanChiShe = document.getElementById("tanChiShe");
+
+
+
+
+    function changeSvg(color) {
+        for (var i = 0; i < svgItems.length; i++) {
+            var paths = svgItems[i].getElementsByTagName("path");
+            for (var j = 0; j < paths.length; j++) {
+                paths[j].setAttribute("fill", color);
+            }
+        }
+    }
+
+
+
+    function changeTheme(theme) {
+        if (theme == "Dark") {
+            themeState = "Dark";
+            changeSvg("#ffffff");
+            tanChiShe.src = "/static/svg/snake-Dark.svg";
+            htmlTag.dataset.theme = 'dack';
+        } else if (theme == "Blue") {
+            themeState = "Blue";
+            changeSvg("#000000");
+            tanChiShe.src = "/static/svg/snake-Light.svg";
+            htmlTag.dataset.theme = '';
+        }
+        setCookie("themeState", theme, 365);
+    }
+
+
+
+
+    function setCookie(name, value, days) {
+        var expires = "";
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+            expires = "; expires=" + date.toUTCString();
+        }
+        document.cookie = name + "=" + value + expires + "; path=/";
+    }
+
+
+    function getCookie(name) {
+        var nameEQ = name + "=";
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i];
+            while (cookie.charAt(0) == ' ') {
+                cookie = cookie.substring(1, cookie.length);
+            }
+            if (cookie.indexOf(nameEQ) == 0) {
+                return cookie.substring(nameEQ.length, cookie.length);
+            }
+        }
+        return null;
+    }
+
+    const switchCheckbox = document.getElementById('myonoffswitch');
+    /*夜间自动打开暗色主题*/
+    const currentTime = new Date();
+    const currentHour = currentTime.getHours();
+    if (currentHour >= 20 || currentHour < 6) {
+        switchCheckbox.checked = false;
+        changeTheme('Dark');
+    }
+
+
+    switchCheckbox.addEventListener('change', function () {
+        if (themeState == "Dark") {
+
+            changeTheme("Blue");
+
+        } else if (themeState == "Blue") {
+
+            changeTheme("Dark");
+        }
+    });
+
+    if (themeState == "Dark") {
+        switchCheckbox.checked = false;
+    }
+    changeTheme(themeState);
+
+
+
+
+    /*淡入效果*/
+    var projectItems = document.querySelectorAll(".projectItem");
+    function checkProjectItems() {
+        for (var i = 0; i < projectItems.length; i++) {
+            var projectItem = projectItems[i];
+            var projectItemTop = projectItem.getBoundingClientRect().top;
+
+            if (projectItemTop < window.innerHeight * 1.2) {
+                projectItem.classList.add("fade-in-visible");
+            }
+        }
+    }
+
+    window.addEventListener("scroll", checkProjectItems);
+    window.addEventListener("resize", checkProjectItems);
+
+
+
+    /*加载效果*/
+    var pageLoading = document.querySelector("#PageLoading");
+    var center = document.getElementById("PageLoading-zyyo-center");
+    setTimeout(function () {
+        checkProjectItems();
+        pageLoading.style.opacity = '0';
+        center.style.height = "500px";
+        center.style.width = "500px";
+        center.style.opacity = "0";
+        pageLoading.style.backgroundSize = "200%";
+    }, 300);
+});
+
+
+/*footer设置 */
+// 动态心跳，更改自己的名称
+$(document).ready(function(e){
+    $('.copyright').html('©2022 - 2023 By 🐟子非鱼');
+})
+
+$(document).ready(function(e){
+    show_date_time();
+})
+
+//本站运行时间，更改自己建立站点的时间
+function show_date_time(){
+$('.framework-info').html('小破站已经安全运行 <span id="span_dt_dt" style="color: #2ac2d1;"></span>');
+window.setTimeout("show_date_time()", 1000);
+BirthDay=new Date("8/15/2023 0:0:0");
+today=new Date();
+timeold=(today.getTime()-BirthDay.getTime());
+sectimeold=timeold/1000
+secondsold=Math.floor(sectimeold);
+msPerDay=24*60*60*1000
+e_daysold=timeold/msPerDay
+daysold=Math.floor(e_daysold);
+e_hrsold=(e_daysold-daysold)*24;
+hrsold=Math.floor(e_hrsold);
+e_minsold=(e_hrsold-hrsold)*60;
+minsold=Math.floor((e_hrsold-hrsold)*60);
+seconds=Math.floor((e_minsold-minsold)*60);
+span_dt_dt.innerHTML='<font style=color:#afb4db>'+daysold+'</font> 天 <font style=color:#f391a9>'+hrsold+'</font> 时 <font style=color:#fdb933>'+minsold+'</font> 分 <font style=color:#a3cf62>'+seconds+'</font> 秒';
+}
